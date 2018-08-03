c7a19be6bcd9d00cb43f2a79dd3d532402b048fe
wesaspinall <wes@wesaspinall.com>
2018-08-03 05:34:24 -0400

    separated header footer and sidebar

--- cat-theme/header.php
+++ cat-theme/header.php
@@ -3,5 +3,17 @@
       <a href="#">
         <?php bloginfo('name'); ?>
       </a>
-    </h1>
+    </h1> 
+    <div class="navcontainer">
+      <div class="inner-navcontainer">
+        <nav class="clearfix">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Cat of the Week</a></li>
+              <li><a href="#">Location</a></li>
+              <li><a href="#">About Us</a></li>
+            </ul>
+        </nav>
+      </div>
+    </div>
 </div><!-- #header --> 
--- cat-theme/index.php
+++ cat-theme/index.php
@@ -14,19 +14,6 @@
   
    <?php get_header(); ?>
 
-    <div class="navcontainer">
-      <div class="inner-navcontainer">
-        <nav class="clearfix">
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Cat of the Week</a></li>
-              <li><a href="#">Location</a></li>
-              <li><a href="#">About Us</a></li>
-            </ul>
-        </nav>
-      </div>
-    </div>
-
 
 
 
@@ -51,71 +38,7 @@
           <span class="post-meta">Posted by <a href="#">Chris Castig</a> on July 20th</span>
         </div><!-- .post --> 
 
-      </div><!-- #main --> 
-
-
-
-
-
-      <div id="sidebar" class="clearfix">
-
-        <div class="widget">
-          <h3>Archive</h3>
-          <ul>
-            <li><a href="#">January</a></li>
-            <li><a href="#">February</a></li>
-            <li><a href="#">March</a></li>
-            <li><a href="#">April</a></li>
-            <li><a href="#">May</a></li>
-            <li><a href="#">June</a></li>
-            <li><a href="#">July</a></li>
-          </ul>
-        </div><!-- .widget --> 
-
-        <div class="widget">
-          <h3>Blogroll</h3>
-          <ul>
-            <li><a href="#">Mailchimp</a></li>
-            <li><a href="#">One Munch</a></li>
-            <li><a href="#">Hacker Mews</a></li>
-            <li><a href="#">Soylent Cat Food</a></li>
-            <li><a href="#">Blue Buffalo</a></li>
-          </ul>
-        </div><!-- .widget --> 
-
-        <div class="widget">
-          <a href="#">
-            <img src="<?php bloginfo(template_directory); ?>/images/ad-submit-your-own.png" alt="Submit Your Own">
-          </a>
-        </div><!-- .widget --> 
-
-        <div class="widget">
-          <a href="#">
-            <img src="<?php bloginfo(template_directory); ?>/images/ad-tshirts.png" alt="Cats Who Code Tees">
-          </a>
-        </div><!-- .widget --> 
-
-
-      </div><!-- #sidebar -->
-
-
-
-
-
-
-      
-      <div class="clear"></div>
-      <div id="footer">
-
-        <a href="https://twitter.com/onemonthedu">
-          <img src="<?php bloginfo(template_directory); ?>/images/social-icon-twitter.png" alt="Twitter">
-        </a>
-
-        <a href="#">
-          <img src="<?php bloginfo(template_directory); ?>/images/social-icon-facebook.png" alt="Facebook">
-        </a>
-        
-      </div><!-- #footer -->
+     <?php get_footer(); ?>
 
     </div><!-- #container --> 
   </body>
